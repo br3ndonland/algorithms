f2bf586c5281ee8e2e721e910635a577cfb66ab6
Brendon Smith <br3ndonland@protonmail.com>
2020-02-14 18:24:47 -0500

    Reformat FizzBuzz comment for JSDoc

--- examples/fizzbuzz.js
+++ examples/fizzbuzz.js
@@ -1,21 +1,24 @@
-/* FizzBuzz
-https://medium.freecodecamp.org/a-software-engineering-survival-guide-fe3eafb47166
-https://medium.freecodecamp.org/coding-interviews-for-dummies-5e048933b82b
-https://stackoverflow.com/a/17623252
-Write a program that prints the numbers from 1 to 100.
-For multiples of three print 'Fizz' instead of the number.
-For multiples of five print 'Buzz' instead of the number.
-For multiples of both three and five print 'FizzBuzz'.
-This solution uses the following syntax features:
-Modulo (%, remainder)
-Strict equality (===), avoiding type coercion (==)
-Addition assignment (+=)
-Boolean OR (||)
-Template strings (``)
-ES6 arrow functions (=>)
-Immediately-invoked function expressions (IIFE)
-*/
-
+/**
+ * ### fizzBuzz
+ * @description
+ * Write a program that prints the numbers from 1 to 100.
+ * For multiples of three print 'Fizz' instead of the number.
+ * For multiples of five print 'Buzz' instead of the number.
+ * For multiples of both three and five print 'FizzBuzz'.
+ *
+ * This solution uses the following syntax features:
+ * - Modulo (`%`, remainder)
+ * - Strict equality (`===`), avoiding type coercion (`==`)
+ * - Addition assignment (`+=`)
+ * - Boolean OR (`||`)
+ * - Template strings (``)
+ * - ES6 arrow functions (`=>`)
+ * - Immediately-invoked function expressions (IIFE)
+ * @tutorial
+ * - [freeCodeCamp](https://medium.freecodecamp.org/a-software-engineering-survival-guide-fe3eafb47166)
+ * - [freeCodeCamp](https://medium.freecodecamp.org/coding-interviews-for-dummies-5e048933b82b)
+ * - [Stack Overflow](https://stackoverflow.com/a/17623252)
+ */
 const fizzBuzz = () => {
   for (let i = 1; i <= 100; i++) {
     let out = ``
