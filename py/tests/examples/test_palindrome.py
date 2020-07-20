509a7c0952141ab62238c1e3e84173654d90b0e3
Brendon Smith <br3ndonland@protonmail.com>
2020-07-20 11:59:41 -0400

    Add tests for palindrome.py
    Note that return statements are necessary to pass Mypy checks.

---
+++ py/tests/examples/test_palindrome.py
@@ -0,0 +1,33 @@
+import pytest
+
+from algorythms.examples import palindrome
+
+
+def test_palindrome_01() -> bool:
+    test = palindrome.prep_input("racecar")
+    assert test is True
+    return test
+
+
+def test_palindrome_02() -> bool:
+    test = palindrome.prep_input("A man, A Plan, A Canal, Panama")
+    assert test is True
+    return test
+
+
+def test_palindrome_03() -> bool:
+    test = palindrome.prep_input("Foo bar")
+    assert test is False
+    return test
+
+
+def test_palindrome_04() -> bool:
+    test = palindrome.prep_input("[]")
+    assert test is False
+    return test
+
+
+def test_palindrome_05() -> bool:
+    with pytest.raises(AttributeError):
+        assert palindrome.prep_input([]) is False  # type: ignore
+        raise
