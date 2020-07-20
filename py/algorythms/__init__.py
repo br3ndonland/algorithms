38f639f319b597eead45a610407469865cffb4f7
Brendon Smith <br3ndonland@protonmail.com>
2020-07-20 11:58:59 -0400

    Calculate version number based on pyproject.toml
    br3ndonland/test3@227ab28
    
    During `poetry install`, Poetry installs the project as if it were a
    package itself. You can then pull the version from the package metadata.
    It's a little tricky to get `importlib_metadata` to work with Docker.
    You have to remove `--no-root` from the _Dockerfile_ and add
    `importlib-metadata` to prod dependencies, and you still might get
    errors after that.
    
    https://github.com/python-poetry/poetry/issues/144
    https://github.com/python-poetry/poetry/issues/1036

--- py/algorythms/__init__.py
+++ py/algorythms/__init__.py
@@ -1,1 +1,23 @@
-__version__ = "0.1.0"
+"""
+algorythms
+---
+Miscellaneous curiosities from the world of computer programming
+https://github.com/br3ndonland/algorithms
+"""
+from importlib.metadata import version
+
+
+def package_version() -> str:
+    """
+    Calculate version number based on pyproject.toml.
+    ---
+    During `poetry install`, Poetry installs the project as if it were a package itself.
+    You can then pull from the package metadata.
+
+    See [poetry#1036](https://github.com/python-poetry/poetry/issues/1036) and
+    [poetry#144](https://github.com/python-poetry/poetry/issues/144) for more info.
+    """
+    return version(__package__)
+
+
+__version__ = package_version()
