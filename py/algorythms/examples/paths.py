a0fb96b718a9b6353da3bcf8521206a02480955d
Brendon Smith <br3ndonland@protonmail.com>
2020-08-12 17:04:11 -0400

    Iteratively import a list of Python modules

--- py/algorythms/examples/paths.py
+++ py/algorythms/examples/paths.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 from pathlib import Path, PurePath
 from typing import List
 
@@ -11,5 +12,17 @@ def get_modules(dirpath: Path = Path(PurePath(__file__).parent)) -> List[str]:
     return [file.name[:-3] for file in dirpath.glob("**/*[!__init__].py")]
 
 
+def import_modules(modules: List[str]) -> List:
+    """Import Python modules in a directory and sub-directories, excluding __init__.py.
+    ---
+    """
+    try:
+        return [import_module(module) for module in modules]
+    except ImportError:
+        raise
+
+
 if __name__ == "__main__":
-    print(get_modules())
+    modules = get_modules()
+    print(modules)
+    import_modules(modules)
