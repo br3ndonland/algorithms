cff4963a1b3c202667dba5135a594c19c192fb1d
Brendon Smith <br3ndonland@protonmail.com>
2020-08-15 09:03:50 -0400

    Improve output of paths.py
    - Sort module list
    - Print the number of modules imported
    - TODO: Path().glob() is not matching all the modules in the directory

--- py/algorythms/examples/paths.py
+++ py/algorythms/examples/paths.py
@@ -7,6 +7,8 @@ def get_modules(directory: Path = Path(__file__).parent) -> List[str]:
     """Get a recursive list of Python modules in a directory, excluding `__init__.py`.
     ---
     https://docs.python.org/3/library/pathlib.html
+
+    TODO: glob is not matching all the modules in this directory
     """
     return [Path(file).stem for file in directory.glob("**/*[!__init__].py")]
 
@@ -23,5 +25,5 @@ def import_modules(modules: List[str]) -> List:
 
 if __name__ == "__main__":
     modules = get_modules()
-    print(modules)
-    import_modules(modules)
+    print(sorted(modules))
+    print(f"{len(import_modules(modules))} modules imported")
