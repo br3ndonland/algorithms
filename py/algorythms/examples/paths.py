6e354aa0a164fbf482e1ae487703f9e5b7be2878
Brendon Smith <br3ndonland@protonmail.com>
2020-08-15 08:43:06 -0400

    Simplify path classes in paths.py
    Just use Path() instead of PurePath()

--- py/algorythms/examples/paths.py
+++ py/algorythms/examples/paths.py
@@ -1,14 +1,14 @@
 from importlib import import_module
-from pathlib import Path, PurePath
+from pathlib import Path
 from typing import List
 
 
-def get_modules(directory: Path = Path(PurePath(__file__).parent)) -> List[str]:
+def get_modules(directory: Path = Path(__file__).parent) -> List[str]:
     """Get a recursive list of Python modules in a directory, excluding `__init__.py`.
     ---
     https://docs.python.org/3/library/pathlib.html
     """
-    return [PurePath(file).stem for file in directory.glob("**/*[!__init__].py")]
+    return [Path(file).stem for file in directory.glob("**/*[!__init__].py")]
 
 
 def import_modules(modules: List[str]) -> List:
