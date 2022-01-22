21ae227dc4673d559a564a9c18a184933d91e1eb
Brendon Smith <bws@bws.bio>
2022-01-22 18:35:00 -0500

    Update dependencies

--- py/algorythms/examples/parse_pyproject_toml.py
+++ py/algorythms/examples/parse_pyproject_toml.py
@@ -1,13 +1,14 @@
 from pathlib import Path
 from typing import Any, Dict
 
-import toml
+import tomli
 
 
 def load_pyproject(pyproject_path: Path = Path("pyproject.toml")) -> Dict[str, Any]:
     """Load pyproject.toml into a dictionary, with a default dict as a fallback."""
     try:
-        return dict(toml.load(pyproject_path))
+        with open(pyproject_path, "rb") as pyproject_bytes:
+            return dict(tomli.load(pyproject_bytes))
     except Exception:
         return {
             "tool": {"poetry": {"name": "algorythms", "description": "", "version": ""}}
